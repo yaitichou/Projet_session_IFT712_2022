f245eaa2b1a93d427d408343480e199d9361227d
yoann19 <pickayo999@gmail.com>
2022-12-03 16:38:33 -0500

    Create data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#####
+# - caiy2401 - CAI, Yunfan
+# - gaye1902 - ElHadji Habib Gaye
+# - aity1101 - AIT ICHOU, Yoann
+###
+
+import numpy as np
+
+
+class dataManager:
+    
+    def __init__(self):
+        return 0
+
+    def data_extract(self):
+        """
+        Method that extracts the data used to train and test
+        our different models
+        """
+        return 0
+
