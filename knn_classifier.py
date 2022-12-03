304120ee6578f7f9cb707419b341ad8dcd64c950
yoann19 <pickayo999@gmail.com>
2022-12-03 15:40:48 -0500

    Init project

---
+++ knn_classifier.py
@@ -0,0 +1,24 @@
+
+
+
+import numpy as np
+import pandas as pd
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+class KNNClassifier(object):
+
+    def __init__(self):
+        return 0
+
+
+
+    def train(self):
+        return 0
+  
+
+    def predict(self, X):
+        return 0
