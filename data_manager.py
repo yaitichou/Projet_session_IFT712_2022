265329304e4987acd9d0a4b68f3993535c562b07
yoann19 <pickayo999@gmail.com>
2022-12-03 18:00:42 -0500

    Init data manager and refactor data files

--- data_manager.py
+++ data_manager.py
@@ -7,17 +7,58 @@
 ###
 
 import numpy as np
+import csv
 
 
-class dataManager:
+
+
+def data_extract(path, dataset = 'train'):
+    """
+    Method that extracts the data used to train and test
+    our different models
+    """
+
+
+    # opening the csv file by specifying
+    # the location
+    # with the variable name as csv_file
+
+    data = []
+    labels = []
+    ids = []
+
+    with open(path,'rt') as csv_file:
+
+        # creating an object of csv reader
+        # with the delimiter as ,
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+
+        # loop to iterate through the rows of csv
+        for row in csv_reader:
+            if(dataset == "train"): # training dataset
+                
+                ids.append(row[0]) # get leaf id
+                labels.append(row[1]) # get species label (second column)
+                data.append(row[2:]) # get data rows without id
+
+            else: # testing dataset
+
+                ids.append(row[0]) # get leaf id
+                data.append(row[1:]) # get data rows without id
+
+
+    attribute_names = data[0] # get the attributes names
+    data = data[1:] # delete attributes names from dataset
+
+    return attribute_names, data, ids, labels
+
+def data_cleaning(data):
+    """
+    Method that extracts the data used to train and test
+    our different models
+    """
     
-    def __init__(self):
-        return 0
+    cleaned_data = data ## To remove
 
-    def data_extract(self):
-        """
-        Method that extracts the data used to train and test
-        our different models
-        """
-        return 0
 
+    return cleaned_data
