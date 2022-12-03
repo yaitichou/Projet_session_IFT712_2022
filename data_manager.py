687a572ba08ab8bbbfb1d4d727dfdfedcd8c701a
yoann19 <pickayo999@gmail.com>
2022-12-03 18:37:45 -0500

    Update data_manager and do preprocessing

--- data_manager.py
+++ data_manager.py
@@ -7,6 +7,7 @@
 ###
 
 import numpy as np
+from sklearn import preprocessing
 import csv
 
 
@@ -16,6 +17,14 @@ def data_extract(path, dataset = 'train'):
     """
     Method that extracts the data used to train and test
     our different models
+    Inputs:
+        - path: the path to the csv file
+        - dataset: the kind of dataset ("train" or "test")
+    Outputs:
+        - attribute_names: the names of the attributes of the data, shape = (A)
+        - data: the data of shape = (N,A) with attributes as columns and leafs as rows (without the labels and the ids)
+        - ids: the ids of the leafs, shape = (N)
+        - labels: the list of expected labels for the leafs, shape = (N)
     """
 
 
@@ -48,9 +57,23 @@ def data_extract(path, dataset = 'train'):
 
 
     attribute_names = data[0] # get the attributes names
-    data = data[1:] # delete attributes names from dataset
+    data = np.array(data[1:]) # delete attributes names from dataset
 
     return attribute_names, data, ids, labels
+
+def error(predictions,labels):
+    """
+    Method that computes the error of a model
+    Inputs :
+        - predictions : the predictions of the dataset, shape = (N)
+        - labels : label of the data, shape = (N)
+    Output :
+        - error : the percentage of predictions that don't match the labels
+
+    """
+
+    return 100 * np.sum(np.abs(predictions - labels)) / len(labels)
+    
 
 def data_cleaning(data):
     """
@@ -58,7 +81,7 @@ def data_cleaning(data):
     our different models
     """
     
-    cleaned_data = data ## To remove
-
+    scaler = preprocessing.StandardScaler().fit(data)
+    cleaned_data = scaler.transform(data)
 
     return cleaned_data
