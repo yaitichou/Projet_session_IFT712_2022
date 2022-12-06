f4b1d6bd78ebd60eb2ca0f7fbd10e71c6ef66f7c
yaitichou <62092385+yaitichou@users.noreply.github.com>
2022-12-06 13:46:24 -0500

    Feature/data manager (#6)
    * Init data manager and refactor data files
    
    * Update data_manager and do preprocessing
    
    * Use data frame to simplify use of data
    
    * Add acp
    
    * Duplicate error fix
    
    * suppress time import

--- data_manager.py
+++ data_manager.py
@@ -6,10 +6,13 @@
 # - aity1101 - AIT ICHOU, Yoann
 ###
 
+import itertools
 import numpy as np
 from sklearn import preprocessing
 import pandas as pd
 import csv
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 
@@ -59,10 +62,12 @@ def data_extract(path, dataset = 'train'):
 
     attribute_names = data[0] # get the attributes names
     data = np.array(data[1:]) # delete attributes names from dataset
-    
+    labels = np.array(labels[1:])
+
     df = pd.DataFrame(data, columns = attribute_names) # get the attributes names
 
     return attribute_names, df, ids, labels
+
 
 def error(predictions,labels):
     """
@@ -89,3 +94,99 @@ def data_cleaning(data):
 
     return cleaned_data
 
+def to_array(data_df):
+    """
+    Method that change a dataframe into an array
+    """
+    data_array = data_df.to_numpy()
+    return data_array
+
+def generate_combinations_for_attributes(attributes_list, max_length):
+    """
+    Method that generate a list of attributes combinations
+
+    Function found here : https://stackoverflow.com/questions/464864/how-to-get-all-possible-combinations-of-a-list-s-elements
+    """
+    generated_combinations = []
+
+    for length_subset in range(np.minimum(len(attributes_list),max_length + 1)):
+        for subset in itertools.combinations(attributes_list, length_subset):
+            generated_combinations.append(subset)
+
+    return generated_combinations
+
+def ACP(data_matrix):
+    """
+    Method that applies the ACP and that plots the results of the ACP
+
+    """
+    n_components = 10 
+    mypca = PCA(n_components=n_components)
+    data = mypca.fit_transform(data_matrix)
+    plt.figure()
+    plt.title("histogramme de la répartition des données selon les "+str(n_components)+" composantes principales")
+    plt.xlabel("composantes")
+    plt.ylabel("variance")
+    
+    #we normalise std for explain that 2 first components are the most representative
+    data = mypca.explained_variance_
+    variance = []
+    min_data = np.amin(data)
+    for i in data:
+        variance.append(i / min_data)
+    value_variance_hist = []
+    value_component = 1
+    for i in variance:
+        j = 0
+        while j != int(i):
+            value_variance_hist.append(value_component)
+            j += 1
+            
+        value_component+=1
+    label, counts = np.unique(value_variance_hist,return_counts=True)
+    plt.bar(label,counts,align="center")
+    
+    #we realise the projection on the 2 first components
+    mypca = PCA(n_components=2)
+
+    
+    data_fitted = mypca.fit_transform(data_matrix)
+    print(mypca.components_)
+
+    data_fitted_x = [element[0] for element in data_fitted]
+    data_fitted_y = [element[1] for element in data_fitted]
+    plt.figure()
+    plt.scatter(data_fitted_x,data_fitted_y)
+    
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("Nuage de point issue d'une analyse en composantes principales")
+    
+    plt.figure()
+    plt.title("Représentation de la corrélation entre les variables note d'un livre")
+    plt.xlabel("PC{}".format(1))
+    plt.ylabel("PC{}".format(2))
+    plt.xlim([-0.5,1])
+    plt.ylim([-1,1])
+
+    #Call the function. 
+    myplot(mypca.components_.T)
+    
+    plt.show()
+
+#function obtained from stackoverflow : https://stackoverflow.com/questions/57340166/how-to-plot-the-pricipal-vectors-of-each-variable-after-performing-pca
+def myplot(coeff,labels=None):
+    """Function permitting to plot correlation of the different variables of a book
+
+    Args:
+        coeff (array): coeff of each vector
+        labels (list, optional): labels for each vector Defaults to None.
+    """
+    n = coeff.shape[0]
+
+    for i in range(n):
+        plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
+        if labels is None:
+            plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, "Var"+str(i+1), color = 'g', ha = 'center', va = 'center')
+        else:
+            plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, labels[i], color = 'g', ha = 'center', va = 'center')
