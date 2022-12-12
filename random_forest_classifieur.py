862153c39222f6b09601afb543c5aa50e815b75c
yunfan <yunfan.cai@usherbrooke.ca>
2022-12-11 22:32:25 -0500

    rf

--- random_forest_classifieur.py
+++ random_forest_classifieur.py
@@ -3,85 +3,127 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold 
-from sklearn.metrics import accuracy_score as ac
-from sklearn.metrics import  precision_score
-from sklearn.metrics import  f1_score
+from IPython.display import display
+
 from sklearn.ensemble import RandomForestClassifier
-
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import classification_report
 
 
 class RandForestClassifier(object):
+    """
+    Class to implement a Random Forest model based on sklearn module.
 
+    Parameters:
+    - x_train (array) -- Array of features values to train on.
+    - y_train (array) -- Array of true labels to train the model.
+    - x_val (array) -- Array of features values to validate the model.
+    - y_val (array) -- Array of true labels to validate the model.
+    - class_names (array) -- Array of names to link with labels
+    """
 
-    def __init__(self, depth = 1,  min_samples_split = 2, n_estimators = 100, criterion = "gini"):
-        self.depth = depth
-        self.random_forest = RandomForestClassifier(random_state=0,criterion=criterion,n_estimators=n_estimators, max_depth=depth, min_samples_split=min_samples_split,)
+    def __init__(self, x_train, y_train, x_val, y_val, class_names, scorers):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_val = x_val
+        self.y_val = y_val
+        self.class_names = class_names
 
+        self.num_features = x_train.shape[1]
+        self.num_classes = class_names.shape
 
-    def train(self,data_train,labels):
-        self.random_forest = self.random_forest.fit(data_train, labels)
-        
-    
-    def cross_validation(self,data_train,labels,depth_list,estimators_list,split_list):
-        grilleRecherche = list()
+        self.estimator = RandomForestClassifier(n_jobs=4)  # nb_cpu-1 for parallelized jobs
+        self.scorers = scorers
+        self.hyper_search = None
 
+    def train_default(self, verbose=False):
+        """
+        Train the model with default sklearn parameters.
 
-        for grid_split in split_list:
-            for grid_depth in depth_list:
-                for grid_estimator in estimators_list:
-                    grilleRecherche.append({"depth": grid_depth,"n_estimators":grid_estimator, "min_samples_split": grid_split})
-        
-        k = 5
+        Prints accuracy for train and validation data.
+        And fit self.estimator with the best scorer
+        """
+        self.estimator.fit(self.x_train, self.y_train)
 
-        kf = KFold(n_splits=k, random_state=None, shuffle=True)
-        accuracy_hyperparameter = {}
-        precision_hyperparameter = {}
-        f1_hyperparameter = {}
-        for hyper in range(len(grilleRecherche)):
-            
-            depth = grilleRecherche[hyper]["depth"]
-            n_estimators = grilleRecherche[hyper]["n_estimators"]
-            min_samples_split = grilleRecherche[hyper]["min_samples_split"]
+        pred = self.estimator.predict(self.x_train)
+        accu_train = accuracy_score(self.y_train, pred)
 
-            random_forest_classifier = RandForestClassifier(depth = depth, n_estimators=n_estimators,min_samples_split=min_samples_split)
-            accuracy = 0
-            precision = 0
-            f1 = 0
-            for train_index, validation_index in kf.split(labels):
+        pred_val = self.estimator.predict(self.x_val)
+        accu_val = accuracy_score(self.y_val, pred_val)
 
-                X_train, X_validation = data_train[train_index], data_train[validation_index]
-                y_train, y_validation = labels[train_index], labels[validation_index]
+        if verbose:
+            print('Accuracy train: {:.3%}'.format(accu_train))
+            print('Accuracy validation: {:.3%}'.format(accu_val))
 
-                random_forest_classifier.train(X_train,y_train) # train the model
+        return accu_train, accu_val
 
-                predictions = random_forest_classifier.predict(X_validation)
-                accuracy += ac(y_validation,predictions) # compute the accuracy
-                precision += precision_score(y_validation,predictions, average = "micro") # compute the precision
-                f1 += f1_score(y_validation,predictions, average = "micro") # compute the precision
-            
-            accuracy_hyperparameter[hyper] = accuracy / k
-            precision_hyperparameter[hyper] = precision / k
-            f1_hyperparameter[hyper] = f1 / k
-            
-        hyper = max(accuracy_hyperparameter, key=accuracy_hyperparameter.get)
-        print("Best hyperparameters:", grilleRecherche[hyper])
+    def train_hyperparameter(self, estimator_params, random_search=True, verbose=False):
+        """
+        Train the model with a grid search and a cross validation
 
-        self.random_forest = RandomForestClassifier(random_state=0, max_depth=grilleRecherche[hyper]["depth"])
-        self.random_forest = self.random_forest.fit(data_train, labels)
+        Inputs:
+        - grid_search_params (dict) -- Dictionnary with values to test in the grid search. If not given, it will use the estimator's default values.
+        - random_search (bool), default=True -- If True use the Randimized Search, 
+                if False search all combination of parameters (take longer time).
 
-        return accuracy_hyperparameter[hyper], f1_hyperparameter[hyper], precision_hyperparameter[hyper]
+        Returns a tuple for:
+        - training accuracy
+        - validation accuracy
+        And
+        - fit self.estimator with the best scorer
+        """
 
+        # Grid search init with kfold
+        searching_params = {
+            "scoring": self.scorers,
+            "refit": "Accuracy",
+            "cv": KFold(n_splits=5, shuffle=True),
+            "return_train_score": True,
+            "verbose": int(verbose),
+            "n_jobs": 4}
+
+        if random_search:
+            if verbose:
+                print("Using randomized search:")
+            self.hyper_search = RandomizedSearchCV(self.estimator, estimator_params).set_params(**searching_params)
+        else:
+            if verbose:
+                print("Using complet search:")
+            self.hyper_search = GridSearchCV(self.estimator, estimator_params).set_params(**searching_params)
+
+        # Search hyper parameters
+        self.hyper_search.fit(self.x_train, self.y_train)
+
+        # Save best estimator
+        self.estimator = self.hyper_search.best_estimator_
+
+        # Predictions on train and validation data
+        pred_train = self.hyper_search.predict(self.x_train)
+        pred_val = self.hyper_search.predict(self.x_val)
+
+        # Train and validation accuracy
+        accu_train = accuracy_score(self.y_train, pred_train)
+        accu_val = accuracy_score(self.y_val, pred_val)
+
+        if verbose:
+            print()
+            print('Best cross val accuracy : {}'.format(self.hyper_search.best_score_))
+            print('Best estimator:\n{}'.format(self.hyper_search.best_estimator_))
+            print()
+            print('Accuracy train: {:.3%}'.format(accu_train))
+            print('Accuracy validation: {:.3%}'.format(accu_val))
+
+        return accu_train, accu_val
 
     def predict(self, X):
+        """
+        Use the trained model to predict the sample's class.
+        X: A list containing one or many samples.
 
-        if len(X.shape) == 1:  # Predict on one sample
-            class_label = np.array([self.random_forest.predict(X)])
-            return class_label
-
-        elif len(X.shape) == 2:  # Predict on multiple samples
-            class_label = []
-            for index in range(X.shape[0]):
-                class_label.append(self.random_forest.predict(X[index].reshape(1,-1))[0])
-            return class_label
+        Returns a encoded class label for each sample.
+        """
+        return self.estimator.predict(X)
